13f21f9a6d99499f96d567eeb99b907ffb8b1f3d
Ballack17 <ballack17@mail.ru>
2020-03-23 13:22:37 +0300

    HW1

--- core/src/ru/gbhwproject/StarTopperHarley.java
+++ core/src/ru/gbhwproject/StarTopperHarley.java
@@ -10,22 +10,25 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 public class StarTopperHarley extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	TextureRegion region;
+	Texture img_BG;
+	//TextureRegion region;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
-		region = new TextureRegion(img, 10,10,15,15);
+		img_BG = new Texture("spaceBG.jpg");
+		//region = new TextureRegion(img, 10,10,15,15);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 0, 0,1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.draw(region,150,150);
+		batch.draw(img_BG,-120,-120);
+		batch.draw(img, 0, 0,75,75);
+		//batch.draw(region,150,150);
 		batch.end();
 	}
 	
@@ -33,5 +36,6 @@ public class StarTopperHarley extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+		img_BG.dispose();
 	}
 }

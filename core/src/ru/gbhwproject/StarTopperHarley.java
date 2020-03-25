c1282b8fa01254510e9abfd62c42dc212c825749
Ballack17 <ballack17@mail.ru>
2020-03-25 11:55:53 +0300

    HW1-2

--- core/src/ru/gbhwproject/StarTopperHarley.java
+++ core/src/ru/gbhwproject/StarTopperHarley.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 
 public class StarTopperHarley extends ApplicationAdapter {
 	SpriteBatch batch;
